4ae3923f5ff02adee202a08ba5fc5ec98fb24b87
young_ogi <duddhrdl2727@naver.com>
2025-02-28 21:33:43 +0900

    feat: 아이디 중복 체크

--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -30,5 +30,12 @@ public class UserService {
                 .role(savedUser.getRole())
                 .build();
     }
+
+    //일반 트랜잭션에서는 JPA가 엔티티의 변경 사항을 감지하기 위해 스냅샷을 저장하고 비교
+    //readOnly=true로 설정하면 이 변경 감지 메커니즘이 비활성화되어 메모리 사용량이 줄고 성능이 향상
+    @Transactional(readOnly = true)
+    public boolean checkDuplicateId(String id) {
+        return userRepository.existsById(id);
+    }
 }
 
